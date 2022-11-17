b9c6f61c2e98bb09dfc27c1b861202a2bd8ce587
BananaMayo <niklas.nygard01@gmail.com>
2022-11-17 16:14:40 +0200

    tht. 5 ja 6 vk. 3

---
+++ viikko3/login-robot/src/services/user_service.py
@@ -0,0 +1,51 @@
+from entities.user import User
+import re
+
+class UserInputError(Exception):
+    pass
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self._user_repository = user_repository
+
+    def check_credentials(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        user = self._user_repository.find_by_username(username)
+
+        if not user or user.password != password:
+            raise AuthenticationError("Invalid username or password")
+
+        return user
+
+    def create_user(self, username, password):
+        self.validate(username, password)
+
+        user = self._user_repository.create(
+            User(username, password)
+        )
+
+        return user
+
+    def validate(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+            #username
+        if not re.match("^[a-z]+$", username):
+            raise UserInputError("Invalid username")
+        if not re.match("^[a-zA-Z][a-zA-Z0-9.-]{3,}[a-zA-Z0-9]$", username):
+            raise UserInputError("Username is too short")
+
+            #password
+        if not re.match("(?=^.{8,}$)", password):
+            raise UserInputError("Password is too short")
+
+        if not re.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", password):
+            raise UserInputError("Must contain at least one special character")
+        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
---
+++ viikko3/login-robot/src/tests/register.robot
@@ -0,0 +1,29 @@
+*** Settings ***
+Resource  resource.robot
+
+*** Test Cases ***
+Register With Valid Username And Password
+    Input Register Command
+    Input Credentials  kalle  kalle123
+    Output Should Contain  New user registered
+
+Register With Already Taken Username And Valid Password
+    Create User  kalle  kalle123
+    Input Register Command
+    Input Credentials  kalle  kalle123
+    Output Should Contain  User with username kalle already exists
+
+Register With Too Short Username And Valid Password
+    Input Register Command
+    Input Credentials  ka  kalle123
+    Output Should Contain  Username is too short
+
+Register With Valid Username And Too Short Password
+    Input Register Command
+    Input Credentials  kalle  kal123
+    Output Should Contain  Password is too short
+
+Register With Valid Username And Long Enough Password Containing Only Letters
+    Input Register Command
+    Input Credentials  kalle  kalkkeaar
+    Output Should Contain  Must contain at least one special character

318e54ae65fa03a108139fba2add10fa50245771
BananaMayo <niklas.nygard01@gmail.com>
2022-11-28 21:08:28 +0200

    vk.4 tht. 5.13 jos koriin lisätään tuote ja se poistetaan niin kori on tyhjä

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -94,3 +94,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.poista_tuote(maito)
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.lukumaara(), 1)
+    
+    def test_korin_tyhjyys_poiston_jalkeen(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        ostos = self.kori.ostokset()
+        self.assertEqual(len(ostos), 0)

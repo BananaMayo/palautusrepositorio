e704f3294ef0aa9ebc618b54480702e8648d0040
BananaMayo <niklas.nygard01@gmail.com>
2022-11-28 20:14:33 +0200

    vk.4 tht. 5.7 kahden saman tuotteen lisääminen näkyy korin hinnassa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -40,3 +40,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_ostoskorissa_on_2_tuotteen_summa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
